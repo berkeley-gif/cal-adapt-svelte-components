13f3a238296b44082c38a46498eb6c37878e5e2e
Chris Henrick <chrishenrick@gmail.com>
2022-03-25 16:32:10 -0700

    test pre-commit hook

--- src/lib/TestComponent/TestComponent.svelte
+++ src/lib/TestComponent/TestComponent.svelte
@@ -7,6 +7,13 @@
   $font-size: 1.25rem;
   $color-strong: magenta;
 
+  div {
+    padding: 1rem;
+    border: 1px solid #333;
+    border-radius: 0.125rem;
+    max-width: clamp(25%, 6rem, 100%);
+  }
+
   p {
     margin: $margin;
     font-size: $font-size;
@@ -17,4 +24,6 @@
   }
 </style>
 
-<p>Hello <strong>{message}</strong>!!!</p>
+<div>
+  <p>Hello <strong>{message}</strong>!!!</p>
+</div>
