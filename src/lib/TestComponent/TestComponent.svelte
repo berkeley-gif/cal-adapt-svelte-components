544d7cca4f3b40d126dc9f4af1f77427033cd758
Chris Henrick <chrishenrick@gmail.com>
2022-03-25 10:51:34 -0700

    test that sass works

--- src/lib/TestComponent/TestComponent.svelte
+++ src/lib/TestComponent/TestComponent.svelte
@@ -2,4 +2,19 @@
 	export let message = "world";
 </script>
 
-<p>Hello <strong>{message}!</strong></p>
+<style lang="scss">
+	$margin: 0;
+	$font-size: 1.25rem;
+	$color-strong: magenta;
+
+	p {
+		margin: $margin;
+		font-size: $font-size;
+	}
+
+	strong {
+		color: $color-strong;
+	}
+</style>
+
+<p>Hello <strong>{message}</strong>!!!</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,15 @@
 	import {TestComponent} from "$lib";
 </script>
 
+<style lang="scss">
+	$font-color: #222;
+
+	:global(body) {
+		font-family: sans-serif;
+		color: $font-color;
+	}
+</style>
+
 <h1>Welcome to Cal-Adapt Svelte Components</h1>
 
 <h2>Test Component</h2>
