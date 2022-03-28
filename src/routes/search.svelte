3c2fffd377ff8ad2b30d42da129d27dc3c6ec976
Chris Henrick <chrishenrick@gmail.com>
2022-03-28 12:38:28 -0700

    move search demo to its own route

---
+++ src/routes/search.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { Search } from "$lib";
+
+  const suggestions = [
+    {
+      id: "one",
+      title: "Option One",
+      value: 1
+    },
+    {
+      id: "two",
+      title: "Option Two",
+      value: 2
+    },
+    {
+      id: "three",
+      title: "Option Three",
+      value: 3
+    }
+  ];
+  const outlineColor = "#036063";
+
+  let searchValue = "";
+  let suggestionsFiltered = suggestions.slice();
+
+  function handleSearchInput(event: Event) {
+    const { value } = <HTMLInputElement>event.target;
+    if (value && value.length >= 3) {
+      searchForItem(value);
+    }
+  }
+
+  function searchForItem(value: string) {
+    suggestionsFiltered = suggestionsFiltered.filter(({ title }) =>
+      title.toLowerCase().includes(value.toLocaleLowerCase())
+    );
+  }
+
+  function handleSearchChange(event: CustomEvent) {
+    console.log(event.detail);
+  }
+
+  function clearSearch() {
+    suggestionsFiltered = suggestions.slice();
+    searchValue = "";
+  }
+</script>
+
+<h1>Seach with auto-suggest</h1>
+<p>This search input will filter suggestions as you type.</p>
+
+<Search
+  on:change="{handleSearchChange}"
+  outlineColor="{outlineColor}"
+  suggestions="{suggestionsFiltered}"
+  searchValue="{searchValue}"
+  handleSearchInput="{handleSearchInput}"
+  clearSearch="{clearSearch}"
+/>
+
+<footer style="margin-top: 10rem">
+  <p><a href="/">Go back to the homepage</a></p>
+</footer>
