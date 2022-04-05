8f8490f0ca2cdb50714784b333ab75c4daf23884
Chris Henrick <chrishenrick@gmail.com>
2022-04-05 11:56:50 -0700

    rm tests dir

--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("index page has expected h1", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.textContent("h1")).toBe(
-    "Welcome to Cal-Adapt Svelte Components"
-  );
-});
