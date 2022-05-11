bd6804443b30c43f4ed5fb15c5f06950a8fa7f14
Chris Henrick <chrishenrick@gmail.com>
2022-05-11 15:49:24 -0700

    stub out modules for StaticMap component

---
+++ src/lib/StaticMap/StaticMap.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  interface Location {
+    id: number;
+    title: string;
+    geometry: {
+      type: "Polygon" | "MultiPolygon" | "Point";
+      coordinates: [number[]] | number[];
+    };
+    center: [number, number];
+    bbox: [number, number, number, number];
+  }
+
+  type MapBoxStyle =
+    | "streets-v11"
+    | "light-v10"
+    | "dark-v10"
+    | "satellite-v9"
+    | "satellite-streets-v11";
+
+  /** specify the height of the map */
+  export let height = 150;
+
+  /** specify the location data */
+  export let location: Location | null = null;
+
+  /** specify the mapbox basemap style type */
+  export let style: MapBoxStyle = "streets-v11";
+
+  /** specify the amount of padding between the overlay and map border */
+  export let padding = 50;
+
+  /** zoom level of the map for point geometries */
+  export let zoom = 8;
+
+  /** wrap the map in an HTML button element or not */
+  export let useButton = true;
+</script>
+
+<div>
+  <pre>
+  {height}
+  {JSON.stringify(location)}
+  {style}
+  {padding}
+  {zoom}
+  {useButton}
+  </pre>
+</div>
---
+++ src/routes/static-map.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { StaticMap } from "$lib";
+</script>
+
+<h1>Static Map</h1>
+<p>
+  The Static Map is intended to be used within a tool's settings panel as a
+  locator map for a location selected by the user.
+</p>
+
+<StaticMap />
