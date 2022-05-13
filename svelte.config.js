a395895bd9080fa08ca0053d9dfd4c89bb8a10cd
Chris Henrick <chrishenrick@gmail.com>
2022-05-13 15:32:49 -0700

    update svelte config: use static site adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { resolve } from "path";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
