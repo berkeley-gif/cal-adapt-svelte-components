305cd0726ad82443572547768ed33d215db06c0f
Chris Henrick <chrishenrick@gmail.com>
2022-05-13 14:45:42 -0700

    added index module to src/data

---
+++ src/data/locations/index.ts
@@ -0,0 +1,7 @@
+import alameda from "./alameda.json";
+import california from "./california.json";
+import censusTract from "./census-tract.json";
+import defaultLocation from "./default-location.json";
+import losAngeles from "./los-angeles.json";
+
+export { alameda, california, censusTract, defaultLocation, losAngeles };
