ac1cfa8e4efe9907d91ef05050bae368c71408b0
Chris Henrick <chrishenrick@gmail.com>
2022-05-16 09:47:10 -0700

    fixed paths in static-map get endpoint

--- src/routes/static-map.ts
+++ src/routes/static-map.ts
@@ -1,15 +1,12 @@
-import { fileURLToPath } from "url";
 import { readdirSync, readFileSync } from "fs";
-import { dirname, extname, join } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { extname, join } from "path";
 
 export async function get() {
-  const locationDir = join(__dirname, "..", "data", "locations");
+  const locationDir = join("static", "data", "locations");
   const locations = readdirSync(locationDir)
     .filter((file) => extname(file) === ".json")
     .map((file) => {
-      const filePath = join(__dirname, "..", "data", "locations", file);
+      const filePath = join("static", "data", "locations", file);
       const data = readFileSync(filePath, "utf-8");
       const json = JSON.parse(data);
       const title = json.title || "unknown";
