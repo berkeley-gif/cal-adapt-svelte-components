802aa89f808c4318bef3bbdfaa8bb2cfddbb2251
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-12 11:35:55 -0500

    update default configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,17 @@
-import { resolve } from "path";
 import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [vitePreprocess()],
 
   kit: {
-    adapter: adapter(),
-    vite: () => ({
-      assetsInclude: ["**/*.json"],
-      resolve: {
-        alias: {
-          "~": resolve("./src"),
-          styles: resolve("./src/styles"),
-          common: resolve("./src/lib/common"),
-          data: resolve("./static/data"),
-          $lib: resolve("./src/lib")
-        }
-      }
-    })
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter()
   }
 };
 
