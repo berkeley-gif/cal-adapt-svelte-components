39478ac34ebdb9405256b82431dc1c2c85032627
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-13 20:25:52 -0500

    simplify layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,79 +1,44 @@
 <script>
   import {
     Header,
-    HeaderUtilities,
-    HeaderAction,
-    HeaderActionLink,
-    HeaderPanelLinks,
-    HeaderPanelLink,
-    HeaderPanelDivider,
     HeaderNav,
     HeaderNavItem,
-    SideNav,
-    SideNavItems,
-    SideNavMenu,
-    SideNavMenuItem,
-    SideNavDivider,
     SkipToContent,
     Content,
     Grid,
     Row,
     Column
   } from "carbon-components-svelte";
-  import LogoGithub from "carbon-icons-svelte/lib/LogoGithub.svelte";
-  import "../styles/main.scss";
 
-  let isOpen = false;
-  let isSideNavOpen = true;
+  import "../styles/main.scss";
 </script>
 
 <Header
   aria-label="Navigation"
   href="{'/'}"
   expandedByDefault="{true}"
-  bind:isSideNavOpen="{isSideNavOpen}"
+  platformName="Cal-Adapt Svelte Components"
 >
   <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
 
-  <HeaderUtilities>
-    <HeaderActionLink
-      icon="{LogoGithub}"
+  <HeaderNav>
+    <HeaderNavItem href="/about" text="About" />
+    <HeaderNavItem
       href="https://github.com/berkeley-gif/cal-adapt-svelte-components"
       target="_blank"
+      text="Github Repo"
     />
-    <HeaderAction transition="{false}" bind:isOpen="{isOpen}">
-      <HeaderPanelLinks>
-        <HeaderPanelDivider>GIF portfolio</HeaderPanelDivider>
-        <HeaderPanelLink
-          href="https://github.com/carbon-design-system/carbon-icons-svelte"
-        >
-          Svelte starter template for GIF projects
-        </HeaderPanelLink>
-        <HeaderPanelLink
-          href="https://github.com/berkeley-gif/caladapt-website-2021"
-        >
-          Cal-Adapt 2.0 using Sapper and Svelte
-        </HeaderPanelLink>
-        <HeaderPanelDivider>Resources</HeaderPanelDivider>
-        <HeaderPanelLink href="https://www.carbondesignsystem.com/">
-          Carbon Design System
-        </HeaderPanelLink>
-        <HeaderPanelLink href="https://www.ibm.com/design/language/">
-          IBM Design Language
-        </HeaderPanelLink>
-      </HeaderPanelLinks>
-    </HeaderAction>
-  </HeaderUtilities>
+  </HeaderNav>
 </Header>
 
-<SideNav bind:isOpen="{isSideNavOpen}">
-  <SideNavItems>
-    <SideNavMenu text="List of Components">
-      <SideNavMenuItem href="/search" text="Search" />
-      <SideNavMenuItem href="/static-map" text="Static Map" />
-    </SideNavMenu>
-    <SideNavDivider />
-  </SideNavItems>
-</SideNav>
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <slot />
+      </Column>
+    </Row>
+  </Grid>
+</Content>
