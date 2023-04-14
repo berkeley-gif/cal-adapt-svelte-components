90a2001790654d346efac3f777832024498ae9a4
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-04-13 21:08:05 -0500

    add description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,21 @@
 <h1>About</h1>
 
-<p>About the library</p>
+<p>
+  TThis library is for developing new UI Components for <a
+    href="https://cal-adapt.org">Cal-Adapt.org</a
+  >. These components use the <a href="https://svelte.dev/">Svelte</a>
+  JavaScript framework and
+  <a href="https://www.carbondesignsystem.com/">IBM Carbon Design</a>system.
+</p>
+
+The rationale for this component repository is:
+<ul>
+  <li>
+    To reduce the time for developing and iterating on UI components for
+    Cal-Adapt.
+  </li>
+  <li>
+    Develop components in isolation so that they may be compatible in other
+    Cal-Adapt related projects.
+  </li>
+</ul>
